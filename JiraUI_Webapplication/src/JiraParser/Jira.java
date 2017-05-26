a023243a30148be5f0d437d39a87c5e509246a40
Raghavendra S S <ssraghavendra15@gmail.com>
2017-05-26 21:41:44 +0530

    Update Jira.java

--- JiraUI_Webapplication/src/JiraParser/Jira.java
+++ JiraUI_Webapplication/src/JiraParser/Jira.java
@@ -31,7 +31,7 @@ public class Jira {
     public static void main(String[] args) throws IOException, InterruptedException {
         Jira jira = new Jira("BICS", "Test", "Test creation 10000884", "raghavendra.s.s.s@oracle.com", "Demo", "4");
         ProcessBuilder ps = new ProcessBuilder("java", "-jar", "C:\\Users\\raghss.ORADEV\\jiraintegration.jar", "create", "-project", jira.project, "-issueType", jira.issueType, "-summary", jira.summary, "-assignee", jira.assignee, "-description", jira.description, "-priority", jira.priority);
-                /*"-project BICSQA (mandatory - always BICSQA, no parameter passed)",
+                /*"-project BICS (mandatory - always BICSQA, no parameter passed)",
                 "-issueType Task (pass parameter, epic, story, task, test)",
                 "-summary  Backlog:Test 12c SAC Installed and running OOTB(blank param by default, but mandatory entry req)",
                 "-assignee andrew.new@oracle.com", "(unassigned by default, otherwise pick owner from list)",
@@ -41,7 +41,7 @@ public class Jira {
 
 //standard output and error output of a subprocess are sent to two
 //separate streams
-        //Process ps = Runtime.getRuntime().exec("java -jar C:\\Users\\raghss.ORADEV\\jiraintegration.jar create -project \"BICSQA\" -issueType \"Task\" -summary \"Backlog:Test Java JIRA JAR OOTB\" -assignee \"andrew.new@oracle.com\" -description \"Based on Dev getting SAC working with 12c, behaviour will be to test to make sure everything works and then enable the tests\"  -priority 1 ");
+        //Process ps = Runtime.getRuntime().exec("java -jar C:\\Users\\raghss.ORADEV\\jiraintegration.jar create -project \"BICS\" -issueType \"Task\" -summary \"Backlog:Test Java JIRA JAR OOTB\" -assignee \"andrew.new@oracle.com\" -description \"Based on Dev getting SAC working with 12c, behaviour will be to test to make sure everything works and then enable the tests\"  -priority 1 ");
         // ProcessBuilder ps = new ProcessBuilder("java", "-jar", "C:\\Users\\raghss.ORADEV\\jiraintegration.jar", "create", "-project","Dummy 123", "-issueType", "Task", "-summary","Dummy1234","-assignee","raghavendra.s.s.s@oracle.com", "-description", "Description", "-priority","1");
 
         ps.redirectErrorStream(true);
